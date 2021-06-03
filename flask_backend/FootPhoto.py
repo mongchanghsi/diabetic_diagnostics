114e399f9cc1a7a947013189b1dcad28794d9edc
mongchanghsi <m.changhsi@gmail.com>
2021-06-03 23:31:44 +0800

    Changed saving file type from jpg to jpeg

--- flask_backend/FootPhoto.py
+++ flask_backend/FootPhoto.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def foot_take_photo():
   now = datetime.now()
   file_date_time = now.strftime("%m%d%Y-%H%M%S")
-  file_name = 'foot/foot_' + file_date_time + '.jpg'
+  file_name = 'foot/foot_' + file_date_time + '.jpeg'
 
   device_input = 'video0'
 
