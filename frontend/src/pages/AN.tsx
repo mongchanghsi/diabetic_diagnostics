1be212a4491dba5ee4a25fe5729be6cd52a131cd
mongchanghsi <m.changhsi@gmail.com>
2021-06-08 13:28:02 +0800

    wip - api integration

--- frontend/src/pages/AN.tsx
+++ frontend/src/pages/AN.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import './temp.css';
+import LoadingGIF from '../assets/loading-gif.gif';
+import { ApiService } from '../utils/api/apiService';
 
 const AN = () => {
   const history = useHistory();
-  const handleClick = () => {
+  const [photoStatus, setPhotoStatus] =
+    useState<'waiting' | 'taking' | 'finished'>('waiting');
+  const handleClick = async () => {
     console.log('clicking');
-    history.push('/foot');
+    setPhotoStatus('taking');
+    const result = await ApiService.take_an();
+    if (result.status === 200) {
+      history.push('/foot');
+    }
+    setPhotoStatus('finished');
   };
 
   return (
     <Card className='detail-card'>
       <Card.Body>
         <Card.Title>Phase 1: Checking for Acanthosis Nigricans</Card.Title>
-        <Card.Text>
-          Please align your head to the marking in blue infront of you.
-        </Card.Text>
-        <Card.Text>
-          Once aligned, please press the button to continue.
-        </Card.Text>
-        <Button onClick={handleClick}>Continue</Button>
+        {photoStatus === 'waiting' && (
+          <>
+            <Card.Text>
+              Please align your head to the marking in blue infront of you.
+            </Card.Text>
+            <Card.Text>
+              Once aligned, please press the button to continue.
+            </Card.Text>
+            <Button onClick={handleClick}>Continue</Button>
+          </>
+        )}
+
+        {photoStatus === 'taking' && <img src={LoadingGIF} alt='loading-gif' />}
+
+        {photoStatus === 'finished' && (
+          <>
+            <Card.Text>
+              Encounter an error, please position again and press continue.
+            </Card.Text>
+            <Button onClick={handleClick}>Continue</Button>
+          </>
+        )}
       </Card.Body>
     </Card>
   );
