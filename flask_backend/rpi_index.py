95eed5ec7fc31bd8e0d4eb6167539609bc2179f3
mongchanghsi <m.changhsi@gmail.com>
2021-06-19 15:13:34 +0800

    changed rpi_index to a different port

--- flask_backend/rpi_index.py
+++ flask_backend/rpi_index.py
@@ -1,27 +1,20 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 from ANPhoto import an_take_photo
 from FootPhoto import foot_take_photo
-from utils.preprocessing import preprocessing
-import tensorflow as tf
-from functools import partial
-import numpy as np
-from sklearn.preprocessing import LabelBinarizer
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-encoder = LabelBinarizer()
-y_data_values = np.array(['AN-POSITIVE', 'AN-NEGATIVE'])
-encoded = encoder.fit_transform(y_data_values)
+CORS(app)
 
 @app.route('/take_an_photo')
 def an_photo():
   file_name = an_take_photo()
-  return file_name
+  return jsonify(file_name)
 
 @app.route('/take_foot_photo')
 def foot_photo():
   file_name = foot_take_photo()
-  return file_name
+  return jsonify(file_name)
 
 if __name__ == "__main__":
-  app.run(host="localhost", port=5001, debug=True)
+  app.run(host="172.20.10.10", port=5001, debug=True)
