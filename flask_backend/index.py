cf5a6d74373dd69dfe7cbd830808b87331351c69
mongchanghsi <m.changhsi@gmail.com>
2021-07-09 13:02:01 +0800

    Fixed tf being able to accept base64 images for prediction

--- flask_backend/index.py
+++ flask_backend/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 from ANPhoto import an_take_photo
 from FootPhoto import foot_take_photo
 from utils.preprocessing import preprocessing
@@ -6,8 +6,13 @@ import tensorflow as tf
 from functools import partial
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
+from flask_cors import CORS
+import base64
+from PIL import Image
+import io
 
 app = Flask(__name__)
+CORS(app)
 
 encoder = LabelBinarizer()
 y_data_values = np.array(['AN-POSITIVE', 'AN-NEGATIVE'])
@@ -19,16 +24,10 @@ def predict_an():
   model = tf.keras.models.load_model('models/an_prediction_model.h5')
 
   # Extract image_file
-  file_name = request.json['file_name']
-  image_file = tf.data.Dataset.list_files(file_name)
+  base64_image = request.json['image']
+  preprocessed_image = preprocessing(base64_image)
 
-  # Run preprocessing on the image_file
-  pf = partial(preprocessing)
-  image_ds = image_file.map(pf)
-
-  # Do prediction
-  for image in image_ds.take(1):
-      img_array = tf.expand_dims(image, 0)
+  img_array = tf.expand_dims(preprocessed_image, 0)
   predictions = model.predict(img_array)
   score = tf.nn.softmax(predictions[0])
 
@@ -40,24 +39,20 @@ def predict_an():
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(classification, 100 * np.max(score))
   )
-  return classification
+  
+  return jsonify(result=classification)
+
 
 @app.route('/predict_foot', methods = ['POST'])
 def predict_foot():
   # Load model
   model = tf.keras.models.load_model('models/foot_prediction_model.h5')
 
-  # Extract image_file
-  file_name = request.json['file_name']
-  image_file = tf.data.Dataset.list_files(file_name)
+# Extract image_file
+  base64_image = request.json['image']
+  preprocessed_image = preprocessing(base64_image)
 
-  # Run preprocessing on the image_file
-  pf = partial(preprocessing)
-  image_ds = image_file.map(pf)
-
-  # Do prediction
-  for image in image_ds.take(1):
-      img_array = tf.expand_dims(image, 0)
+  img_array = tf.expand_dims(preprocessed_image, 0)
   predictions = model.predict(img_array)
   score = tf.nn.softmax(predictions[0])
 
@@ -69,7 +64,9 @@ def predict_foot():
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(classification, 100 * np.max(score))
   )
-  return classification
+  
+  return jsonify(result=classification)
 
 if __name__ == "__main__":
+  # Alternatively 172.20.10.4
   app.run(host="localhost", port=5000, debug=True)
--- flask_backend/utils/preprocessing.py
+++ flask_backend/utils/preprocessing.py
@@ -1,13 +1,22 @@
 import tensorflow as tf
+import numpy as np
 
 IMAGE_SIZE = 128
 
-def preprocessing(filename, width = IMAGE_SIZE, height = IMAGE_SIZE):
-    #Retrieve image
-    image_string = tf.io.read_file(filename)
-    img = tf.image.decode_jpeg(image_string, channels=3) #channels set to 1 as it is greyscale
+def adjustBase64Image(image):
+    image = image.replace('+', '-')
+    image = image.replace('/', '_')
+    return image
+
+def preprocessing(base64_image, width = IMAGE_SIZE, height = IMAGE_SIZE):
+    #Decode base64 image
+    img = adjustBase64Image(base64_image)
+    img = tf.io.decode_base64(img)
+    img = tf.io.decode_jpeg(img, channels=3)
+
     #Normalise the pixel values from 0-255 to 0-1
     img = tf.image.convert_image_dtype(img, tf.float32)
+
     #Resize the image
     img = tf.image.resize(img, [height, width])
     return img
