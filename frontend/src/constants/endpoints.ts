7714b85b86ee6023144367ff8246558a7501938e
mongchanghsi <m.changhsi@gmail.com>
2021-07-09 13:12:41 +0800

    Added new endpoints

--- frontend/src/constants/endpoints.ts
+++ frontend/src/constants/endpoints.ts
@@ -5,15 +5,17 @@ interface IEndpoints {
   TAKE_FOOT: string;
   PREDICT_AN: string;
   PREDICT_FOOT: string;
+  SAVE_IMAGES: string;
 }
 
 const endpoints: IEndpoints = {
   LOGIN: '/api/user/login',
-  UPDATE: '/api/user/update',
+  UPDATE: '/api/user/update_details',
   TAKE_AN: '/take_an_photo',
   TAKE_FOOT: '/take_foot_photo',
   PREDICT_AN: '/predict_an',
   PREDICT_FOOT: '/predict_foot',
+  SAVE_IMAGES: '/api/image/save_image',
 };
 
 export default endpoints;
--- frontend/src/utils/api/apiService.ts
+++ frontend/src/utils/api/apiService.ts
@@ -91,4 +91,90 @@ export class ApiService {
       };
     }
   }
+
+  static async save_images(
+    nric: string,
+    an_base64: string,
+    left_foot_base64: string,
+    right_foot_base64: string
+  ) {
+    try {
+      const result = await fetch(
+        `${config.api_endpoint}${endpoints.SAVE_IMAGES}`,
+        {
+          ...requestOptions_POST,
+          body: JSON.stringify({
+            nric,
+            an_base64,
+            left_foot_base64,
+            right_foot_base64,
+          }),
+        }
+      );
+      const content = await result.json();
+      return {
+        status: result.status,
+        data: content,
+        error: null,
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        data: 'Internal Server Error',
+        error: error.message,
+      };
+    }
+  }
+
+  static async predict_an(image: string) {
+    try {
+      const result = await fetch(
+        `${config.flask_endpoint}${endpoints.PREDICT_AN}`,
+        {
+          ...requestOptions_POST,
+          body: JSON.stringify({
+            image,
+          }),
+        }
+      );
+      const content = await result.json();
+      return {
+        status: result.status,
+        data: content.result,
+        error: null,
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        data: 'Internal Server Error',
+        error: error.message,
+      };
+    }
+  }
+
+  static async predict_foot(image: string) {
+    try {
+      const result = await fetch(
+        `${config.flask_endpoint}${endpoints.PREDICT_FOOT}`,
+        {
+          ...requestOptions_POST,
+          body: JSON.stringify({
+            image,
+          }),
+        }
+      );
+      const content = await result.json();
+      return {
+        status: result.status,
+        data: content.result,
+        error: null,
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        data: 'Internal Server Error',
+        error: error.message,
+      };
+    }
+  }
 }
